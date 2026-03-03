44811ca7d72e0a962ca067cfc984082e55181383
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2026-03-03 20:55:13 -0300

    incluiopen api

--- src/main/java/dev/javacourse/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/javacourse/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,4 +1,5 @@
 package dev.javacourse.CadastroDeNinjas.Ninjas;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,8 @@ private final NinjaService ninjaService;
         this.ninjaService = ninjaService;
     }
 //add ninja
-
+    @Operation(summary = "Criar ninja",
+        description = "Cria o ninja no sistema")
     @PostMapping("/criar")
     public ResponseEntity<?> criarNinja(@RequestBody NinjaDTO ninja){
         NinjaDTO novoninja =  ninjaService.criarNinja(ninja);
@@ -28,17 +30,15 @@ private final NinjaService ninjaService;
                 .body(novoninja);
     }
 
-    //Mostrar todos os ninjas
-
+    @Operation(summary = "Lista ninjas",
+            description = "Lista todos os ninjas cadastrados")
     @GetMapping("/listar")
     public List<NinjaDTO> listarNinjas(){
-//        List<NinjaDTO> listaNinja = ninjaService.listarNinjas();
         return ninjaService.listarNinjas();
     }
 
-
-    //procurar ninja por id
-
+    @Operation(summary = "Lista ninjas por ID",
+            description = "Lista o ninja correspondente ao ID")
     @GetMapping("/listar/{id}")
     public NinjaDTO buscaNinja(@PathVariable Long id){
         NinjaDTO ninja = ninjaService.listarNinjasPorid(id);
@@ -46,7 +46,8 @@ private final NinjaService ninjaService;
     }
 
     //alterar ninja
-
+    @Operation(summary = "Altera ninjas por ID",
+            description = "altera o ninja correspondente ao ID")
     @PutMapping("/alterar/{id}")
     public ResponseEntity<?> alteraNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado){
         Map<String, Object> resposta = new LinkedHashMap<>();
@@ -62,7 +63,8 @@ private final NinjaService ninjaService;
     }
 
 
-    //deletar ninja
+    @Operation(summary = "Deleta ninjas por ID",
+            description = "Deleta o ninja correspondente ao ID")
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<?> deletaNinja(@PathVariable Long id){
         if(ninjaService.listarNinjasPorid(id) != null ){
